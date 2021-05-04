314150aab51f9b5b8b2eda88cbb2c93dd0da2777
Rodrigo Abreu <rodrigo.s.abreu@gmail.com>
2021-05-03 22:22:18 -0300

    output decorator

---
+++ src/app/app.routes.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './navegacao/home/home.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { CadastroComponent } from './demos/reactiveforms/cadastro/cadastro.component';
+
+//export const rootRouterConfig: Routes = [
+const rootRouterConfig: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent},
+    { path: 'sobre', component: SobreComponent },
+    { path: 'cadastro', component: CadastroComponent },
+    //abordagem lazy loading de módulos
+    { path: 'produtos', 
+            loadChildren: () => import('./demos/arquitetura-componentes/produto.module')
+            .then(m => m.ProdutoModule)
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(rootRouterConfig)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule{}
